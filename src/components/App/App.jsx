ce88bc9d81784247910952d18a2d3858461a339b
Oleg Apostol <truerenton@gmail.com>
2017-12-13 10:32:34 +0300

    🌈  Theming

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { BrowserRouter as Router, Route, withRouter, Switch } from 'react-router'
+import {
+    createMuiTheme,
+    MuiThemeProvider } from 'material-ui/styles'
+import { teal } from 'material-ui/colors'
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    withRouter } from 'react-router'
 import PageTransition from 'react-router-page-transition'
 
 import { request } from '../../utils'
@@ -9,6 +17,12 @@ import { setPeoples } from '../../actions'
 import './App.styl'
 import Search from '../Search/Search.jsx'
 import People from '../People/People.jsx'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: teal
+    }
+})
 
 @withRouter
 @connect(() => ({}))
@@ -24,14 +38,16 @@ class App extends Component {
         const { location } = this.props
 
         return (
-            <div className='App'>
-                <PageTransition timeout={200}>
-                    <Switch location={location}>
-                        <Route exact path='/' component={Search} />
-                        <Route path='/people/:id' component={People} />
-                    </Switch>
-                </PageTransition>
-            </div>
+            <MuiThemeProvider theme={theme}>
+                <div className='App'>
+                    <PageTransition timeout={200}>
+                        <Switch location={location}>
+                            <Route exact path='/' component={Search} />
+                            <Route path='/people/:id' component={People} />
+                        </Switch>
+                    </PageTransition>
+                </div>
+            </MuiThemeProvider>
         )
     }
 }
